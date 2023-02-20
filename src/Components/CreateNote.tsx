4bafcd3e0580311f1cfaf89b3d710c833708c25c
Rahuman <rahuinfinitive@gmail.com>
2023-02-20 21:20:40 +0530

    Fixing issues

--- src/Components/CreateNote.tsx
+++ src/Components/CreateNote.tsx
@@ -1,31 +1,55 @@
-import * as React from 'react';
-import {Form,Button} from 'react-bootstrap';
+import {Form,Button, Alert} from 'react-bootstrap';
+import React, { useRef } from 'react';
+import { Note } from '../Models/note.model';
 
 
 interface ICreateNotesProps {
+  notes: Note[],
+  setNotes: React.Dispatch<React.SetStateAction<Note[]>>
 }
 
-const CreateNotes: React.FunctionComponent<ICreateNotesProps> = (props) => {
+const CreateNotes: React.FunctionComponent<ICreateNotesProps> = ({notes,setNotes}) => {
+  const [error,setError] =React.useState<string>("")
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+  const colorRef = useRef<HTMLInputElement | null>(null);
+  const handleSubmit  =(e: React.FormEvent<HTMLFormElement>):void => {
+    e.preventDefault();
+    if(titleRef.current?.value ===  "" || textRef.current?.value === "")
+    {
+      return setError('All fields are madatory')
+    }
+    setError("");
+    setNotes([...notes,{
+      id:(new Date()).toString(),
+      title:(titleRef.current as HTMLInputElement).value,
+      text:(textRef.current as HTMLTextAreaElement).value,
+      color:(colorRef.current as HTMLInputElement).value,
+      date:(new Date()).toString()
+    }])
+
+  }
   return (
     <>
     <h1>Create Notes</h1>
-    <Form className='mb-3 mt-3'>
+    {error && <Alert variant="danger">{error}</Alert>}
+    <Form className='mb-3 mt-3' onSubmit={(e) => handleSubmit(e)}>
         <Form.Group className='mb-3' controlId='formBasicTitle'>
             <Form.Label>Title</Form.Label>
-            <Form.Control type='text' placeholder='Enter Title for the Note'>
-            </Form.Control>
+            <Form.Control type='text' placeholder='Enter Title for the Note' ref = {titleRef}/>
+            
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicText'>
             <Form.Label>Text</Form.Label>
-            <Form.Control placeholder='Enter your notes' as="textarea" rows={3}>
-            </Form.Control>
+            <Form.Control placeholder='Enter your notes' as="textarea" rows={3} ref = {textRef}/>
+            
         </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicTitle'>
             <Form.Label htmlFor='colorInput'>Notes Color</Form.Label>
-            <Form.Control type='color' id='colorInput' title='choose the color'>
-            </Form.Control>
+            <Form.Control type='color'  id='colorInput' title='choose the color' ref = {colorRef}/>
+            
         </Form.Group>
-        <Button type='submit' variant='primary'>Submit</Button>
+        <Button  type="submit" variant='primary'>Submit</Button>
     </Form>
     </>
   );
